f5c92d9c29e30b6927e3f4c6bdb1fb1320f0db21
HaiHeHuoc <85790429+HaiHeHuoc@users.noreply.github.com>
2026-07-19 15:20:49 +0700

    Temp save wifi config

--- components/config_manager/include/config_manager.h
+++ components/config_manager/include/config_manager.h
@@ -1,9 +1,43 @@
 #pragma once
 
+/* Includes ----------------------------------------------------------------- */
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "esp_err.h"
+
+/* Macros ------------------------------------------------------------------- */
+#define CONFIG_MANAGER_WIFI_SSID_MAX_LEN 32U
+#define CONFIG_MANAGER_WIFI_PASSWORD_MAX_LEN 63U
+
+#define CONFIG_MANAGER_WIFI_SSID_BUFFER_SIZE \
+    (CONFIG_MANAGER_WIFI_SSID_MAX_LEN + 1U)
+
+#define CONFIG_MANAGER_WIFI_PASSWORD_BUFFER_SIZE \
+    (CONFIG_MANAGER_WIFI_PASSWORD_MAX_LEN + 1U)
+
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
+    /* Type Definitions --------------------------------------------------------- */
+    typedef struct
+    {
+        char ssid[CONFIG_MANAGER_WIFI_SSID_BUFFER_SIZE];
+        char password[CONFIG_MANAGER_WIFI_PASSWORD_BUFFER_SIZE];
+    } config_manager_wifi_config_t;
+
+    /* Functions ---------------------------------------------------------------- */
+    esp_err_t config_manager_init(void);
+
+    esp_err_t config_manager_save_wifi(
+        const config_manager_wifi_config_t *config);
+
+    esp_err_t config_manager_load_wifi(
+        config_manager_wifi_config_t *config);
+
+    esp_err_t config_manager_clear_wifi(void);
 #ifdef __cplusplus
 }
 #endif
